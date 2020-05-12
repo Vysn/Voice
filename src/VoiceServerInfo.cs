312071262efe36ee4ca4a89f6048577103e58639
Yucked <yucked@protonmail.com>
2020-05-12 13:38:38 -0700

    Let's start working on the project

---
+++ src/VoiceServerInfo.cs
@@ -0,0 +1,7 @@
+﻿namespace Vysn.Voice {
+	public struct VoiceServerInfo {
+		public string SessionId { get; private set; }
+		public string Endpoint { get; private set; }
+		public string Token { get; private set; }
+	}
+}
