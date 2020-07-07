aff27d00d9c04e5e4e1f3b49be3ffae905e3e3b3
Yucked <yucked@protonmail.com>
2020-07-06 22:14:53 -0400

    Rest of the stuff

--- src/VoiceServerInfo.cs
+++ src/VoiceServerInfo.cs
@@ -1,7 +1,21 @@
 ﻿namespace Vysn.Voice {
+	/// <summary>
+	/// Information required to connect to ws server.
+	/// </summary>
 	public struct VoiceServerInfo {
+		/// <summary>
+		/// Voice channel session id.
+		/// </summary>
 		public string SessionId { get; private set; }
+		
+		/// <summary>
+		/// Endpoint of voice channel's voice server.
+		/// </summary>
 		public string Endpoint { get; private set; }
+		
+		/// <summary>
+		/// Voice channel's connection token.
+		/// </summary>
 		public string Token { get; private set; }
 	}
 }
