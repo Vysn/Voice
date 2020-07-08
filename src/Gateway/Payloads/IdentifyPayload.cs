0107c5c08816e90837dee9b830cb97a26fb86dd6
Yucked <yucked@protonmail.com>
2020-07-07 22:19:38 -0400

    2 basic payloads

---
+++ src/Gateway/Payloads/IdentifyPayload.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+using Vysn.Commons;
+
+namespace Vysn.Voice.Gateway.Payloads {
+    internal struct IdentifyPayload {
+        [JsonPropertyName("guild_Id")]
+        public Snowflake GuildId { get; set; }
+
+        [JsonPropertyName("user_id")]
+        public Snowflake UserId { get; set; }
+
+        [JsonPropertyName("session_id")]
+        public string SessionId { get; set; }
+
+        [JsonPropertyName("token")]
+        public string Token { get; set; }
+    }
+}
---
+++ src/Gateway/Payloads/ReadyPayload.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Vysn.Voice.Gateway.Payloads {
+    internal struct ReadyPayload {
+        [JsonPropertyName("ssrc")]
+        public int SSRC { get; set; }
+
+        [JsonPropertyName("ip")]
+        public string Ip { get; set; }
+
+        [JsonPropertyName("port")]
+        public int Port { get; set; }
+
+        [JsonPropertyName("modes")]
+        public string[] Modes { get; set; }
+
+        [JsonPropertyName("heartbeat_interval")]
+        public int Interval { get; set; }
+    }
+}
