f32718eecf6dd226a8c2c5d4d459d142c9e8a8f0
Yucked <yucked@protonmail.com>
2020-07-07 22:19:57 -0400

    Deserialize this baby in ctor with JsonDocument

---
+++ src/Gateway/Payloads/GatewayPayload.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.Json;
+using Vysn.Voice.Gateway.Enums;
+
+namespace Vysn.Voice.Gateway.Payloads {
+    internal readonly struct GatewayPayload {
+        public OpCode OpCode { get; }
+        public object OpData { get; }
+
+        public GatewayPayload(OpCode opCode, object opData) {
+            OpCode = opCode;
+            OpData = opData;
+        }
+
+        public GatewayPayload(byte[] data) {
+            using var document = JsonDocument.Parse(data);
+            var rootElement = document.RootElement;
+
+            if (!rootElement.TryGetProperty("op", out var opElement)) {
+                throw new Exception("Missing op property from payload.");
+            }
+
+            OpCode = (OpCode) opElement.GetInt32();
+            if (!rootElement.TryGetProperty("d", out var dataElement)) {
+                throw new Exception("Missing data property from payload.");
+            }
+
+            OpData = OpCode switch {
+                OpCode.Ready => new ReadyPayload {
+                    SSRC = rootElement.GetProperty("ssrc").GetInt32(),
+                    Ip = rootElement.GetProperty("ip").GetString(),
+                    Port = rootElement.GetProperty("port").GetInt32(),
+                    Modes = default,
+                    Interval = rootElement.GetProperty("interval").GetInt32()
+                }
+            };
+        }
+    }
+}
