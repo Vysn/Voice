7594e225d102032359d203d294ef5d71ccdb9b2f
Yucked <yucked@protonmail.com>
2020-07-08 17:45:53 -0400

    Added SelectPayload.cs

---
+++ src/Gateway/Payloads/SelectPayload.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Vysn.Voice.Gateway.Payloads {
+    internal struct SelectPayload {
+        [JsonPropertyName("protocol")]
+        public string Protocol { get; set; }
+
+        [JsonPropertyName("data")]
+        public object Data { get; set; }
+    }
+}
