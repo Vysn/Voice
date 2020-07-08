4070518b8ca0d7672ed576a13d265100351a3ab5
Yucked <yucked@protonmail.com>
2020-07-08 16:29:30 -0400

    I'm cooking something nice worse than rice

--- src/Gateway/Payloads/GatewayPayload.cs
+++ src/Gateway/Payloads/GatewayPayload.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using Vysn.Voice.Gateway.Enums;
 
 namespace Vysn.Voice.Gateway.Payloads {
     internal readonly struct GatewayPayload {
+        [JsonPropertyName("op")]
         public OpCode OpCode { get; }
+
+        [JsonPropertyName("d")]
         public object OpData { get; }
 
         public GatewayPayload(OpCode opCode, object opData) {
@@ -30,10 +34,41 @@ namespace Vysn.Voice.Gateway.Payloads {
                     SSRC = rootElement.GetProperty("ssrc").GetInt32(),
                     Ip = rootElement.GetProperty("ip").GetString(),
                     Port = rootElement.GetProperty("port").GetInt32(),
-                    Modes = default,
+                    Modes = GetModes(rootElement),
                     Interval = rootElement.GetProperty("interval").GetInt32()
+                },
+                OpCode.Hello => new HelloPayload {
+                    Interval = rootElement.GetProperty("heartbeat_interval").GetInt32()
+                },
+                OpCode.SessionDescription => new SessionDescriptionPayload {
+                    Mode = rootElement.GetProperty("mode").GetString(),
+                    SecretKey = GetSecretKey(rootElement)
                 }
             };
+        }
+
+        private static string[] GetModes(JsonElement jsonElement) {
+            var element = jsonElement.GetProperty("modes");
+            var elementArray = new string[element.GetArrayLength()];
+            var i = 0;
+            foreach (var item in element.EnumerateArray()) {
+                elementArray[i] = element.GetString();
+                i++;
+            }
+
+            return elementArray;
+        }
+
+        private static byte[] GetSecretKey(JsonElement jsonElement) {
+            var element = jsonElement.GetProperty("secret_key");
+            var elementArray = new byte[element.GetArrayLength()];
+            var i = 0;
+            foreach (var item in element.EnumerateArray()) {
+                elementArray[i] = element.GetByte();
+                i++;
+            }
+
+            return elementArray;
         }
     }
 }
