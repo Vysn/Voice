4648436bda4a1e345ba5857cde7a31e15ae02420
Yucked <yucked@protonmail.com>
2020-07-09 15:26:56 -0400

    CodecType.cs enum (Audio/Video)

---
+++ src/Codec/Enums/CodecType.cs
@@ -0,0 +1,16 @@
+namespace Vysn.Voice.Codec.Enums {
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum CodecType {
+        /// <summary>
+        /// 
+        /// </summary>
+        Audio,
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        Video
+    }
+}
